298d3005e847c611e519796aa01cc85ff705f4be
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2026-06-02 16:03:51 +0200

    [feat/US11] accep & decline demande d'ami

--- src/main/java/com/bschool/msgrestapi/service/impl/FriendRequestServiceImpl.java
+++ src/main/java/com/bschool/msgrestapi/service/impl/FriendRequestServiceImpl.java
@@ -1,8 +1,10 @@
 package com.bschool.msgrestapi.service.impl;
 
+import com.bschool.msgrestapi.domain.entity.Conversation;
 import com.bschool.msgrestapi.domain.entity.User;
 import com.bschool.msgrestapi.domain.enums.FriendRequestStatus;
 import com.bschool.msgrestapi.exception.BusinessException;
+import com.bschool.msgrestapi.repository.ConversationRepository;
 import com.bschool.msgrestapi.repository.FriendRequestRepository;
 import com.bschool.msgrestapi.repository.UserRepository;
 import com.bschool.msgrestapi.service.FriendRequestService;
@@ -19,6 +21,9 @@ public class FriendRequestServiceImpl implements FriendRequestService {
     UserRepository userRepository;
     @Autowired
     FriendRequestRepository friendRequestRepository;
+
+    @Autowired
+    ConversationRepository conversationRepository;
 
     @Override
     public FriendRequest sendRequest(Long senderId, Long receiverId) {
@@ -50,12 +55,39 @@ public class FriendRequestServiceImpl implements FriendRequestService {
 
     @Override
     public FriendRequest accept(Long requestId, Long receiverId) {
-        throw new BusinessException("À implémenter — US5 + création discussion (Loïc / Charbel)");
+        FriendRequest friendRequest = friendRequestRepository.getOne(requestId);
+        if(!friendRequest.getReceiver().getId().equals(receiverId)){
+            throw new BusinessException("L'utilisateur n'est pas le destinataire de la demande d'ami");
+        }
+        //Mettre à jour le statut de la demande d'ami
+        friendRequest.setStatus(FriendRequestStatus.ACCEPTED);
+        friendRequest.setRespondedAt(java.time.Instant.now());
+
+        //Creation de la discussion et de l'amitié
+        Conversation conversation = new Conversation();
+        if(friendRequest.getReceiver().getId() > friendRequest.getSender().getId()){
+            conversation.setParticipantLow(friendRequest.getSender());
+            conversation.setParticipantHigh(friendRequest.getReceiver());
+        }else {
+            conversation.setParticipantLow(friendRequest.getReceiver());
+            conversation.setParticipantHigh(friendRequest.getSender());
+        }
+        conversation.setCreatedAt(java.time.Instant.now());
+
+        conversationRepository.save(conversation);
+        return friendRequestRepository.save(friendRequest);
     }
 
     @Override
     public FriendRequest decline(Long requestId, Long receiverId) {
-        throw new BusinessException("À implémenter — US11 (Sabine)");
+        FriendRequest friendRequest = friendRequestRepository.getReferenceById(requestId);
+        if (!friendRequest.getReceiver().getId().equals(receiverId)) {
+            throw new BusinessException("L'utilisateur n'est pas le destinataire de la demande d'ami");
+        }
+        friendRequest.setStatus(FriendRequestStatus.DECLINED);
+        friendRequest.setDeclineAt(java.time.Instant.now());
+        return friendRequestRepository.save(friendRequest);
+        //throw new BusinessException("À implémenter — US11 (Sabine)");
     }
 
     @Override
