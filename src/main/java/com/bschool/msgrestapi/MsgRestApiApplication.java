7a2bf3f898a96ddd31159a48d4b79abb8a6b9908
Charbel Sonon <104888004+AsKing07@users.noreply.github.com>
2026-06-02 09:10:22 +0200

    First Commit

---
+++ src/main/java/com/bschool/msgrestapi/MsgRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bschool.msgrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bschool/msgrestapi/MsgRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bschool.msgrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
